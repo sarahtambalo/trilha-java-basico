02cb6c46f67452f429530e458c08bd66b8380c1f
Sarah Tambalo <sarahbianca90@gmail.com>
2024-06-04 18:05:04 -0300

    adicionando exemplos de aula

---
+++ java-anatomia-classes/src/edu/sarah/primeirasemana/MinhaClasse.java
@@ -0,0 +1,17 @@
+package edu.sarah.primeirasemana;
+public class MinhaClasse {
+
+public static void main (String [] args) {
+    String primeiroNome = "Sarah";
+    String segundoNome = "Tambalo";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+    
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
